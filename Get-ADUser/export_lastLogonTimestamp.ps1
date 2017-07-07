33fc618c96b1fd4ec08de7a4b5f665ae1ef7afa1
gbouziden <george.bouziden@gmail.com>
2017-07-07 12:57:05 -0700

    Update export_lastLogonTimestamp.ps1

--- Get-ADUser/export_lastLogonTimestamp.ps1
+++ Get-ADUser/export_lastLogonTimestamp.ps1
@@ -1,7 +1,7 @@
 # This CmdLet selects a spcific Organizational Unit and outputs each users Last Logon Date
 # If they have never logged in, the date will show up as 12/31/1600 4:00:00 PM
 
-# Your SearchBase might be different, but this one matches an OU in an OU at a name@dept.school.edu
+# Your SearchBase might be different, but this one matches an OU in an OU at a name@**dept.school.edu**
 # The best way to get the exact SearchBase is OU Properties > Attribute Editor > Dbl-Clk and Copy distinguishedName
 
 # You can also edit output to whatever directory and change file type.
