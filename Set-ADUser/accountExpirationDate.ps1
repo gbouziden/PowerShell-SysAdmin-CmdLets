b6dba773708f224f2d780d1549886a64c9ba3355
gbouziden <george.bouziden@gmail.com>
2017-07-11 10:34:16 -0700

    Add files via upload

---
+++ Set-ADUser/accountExpirationDate.ps1
@@ -0,0 +1,6 @@
+# This is a pretty standard way to set an account expiration date for a user
+# It give you a litle more control because you can set the specific time of day as well as date.
+
+PS C:\Windows\system32> Set-ADAccountExpirationDate username -DateTime "12/31/2020 5:00:00 PM"
+
+
